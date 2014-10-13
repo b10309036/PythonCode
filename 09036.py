5f96d1130c67a7c0406d8091bda6a04c09dcd4d8
b10309036 <life841003@yahoo.com.tw>
2014-10-13 14:39:32 +0800

    if
    if

---
+++ 09036.py
@@ -0,0 +1,12 @@
+#if else
+x = int(input("Please enter an integer:"))
+
+if x < 0:
+    x = 0
+    print("Negative")
+elif x == 0:
+    print("Orezo")
+elif x == 1:
+    print("Double")
+else:
+    print("Better")
